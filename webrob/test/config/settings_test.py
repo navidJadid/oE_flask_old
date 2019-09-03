32dce9b1e29d07bc6a692c90f21ea5c18dc71fc5
Rollson <rollsonipo@gmail.com>
2019-09-03 16:29:29 +0200

    test(openease_flask/webrob/test/config/settings_test.py): add test for retrieve_mail_server method

--- webrob/test/config/settings_test.py
+++ webrob/test/config/settings_test.py
@@ -3,10 +3,12 @@ import pytest
 from webrob.config.settings import Config
 
 MAIL_PASSWORD = 'abc'
+MAIL_SERVER = 'smtp.gmail.commm' # working now
 
 @pytest.fixture
 def monkeypatch_setup(monkeypatch):
     monkeypatch.setenv('OPENEASE_MAIL_PASSWORD', MAIL_PASSWORD)
+    monkeypatch.setenv('OPENEASE_MAIL_SERVER', MAIL_SERVER)
 
     # TODO: mock all other environment variables, so it does not need to be done twice
     return monkeypatch
@@ -25,3 +27,8 @@ def test_retrieve_mail_password(monkeypatch_setup):
 def test_retrieve_mail_password_error():
     with pytest.raises(KeyError):
         Config._retrieve_mail_password()
+
+
+def test_retrieve_mail_server(monkeypatch_setup):
+    Config._retrieve_mail_server()
+    assert Config.MAIL_SERVER == MAIL_SERVER
