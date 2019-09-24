afc0ed129d3286c9ad1eac723ac5c41fb97a74f1
Rollson <rollsonipo@gmail.com>
2019-09-24 13:51:41 +0200

    test(settings_test.py): add test to retrieve google tokens error

--- webrob/test/config/settings_test.py
+++ webrob/test/config/settings_test.py
@@ -148,6 +148,10 @@ def test_retrieve_google_tokens(monkeypatch_setup):
     Config._retrieve_google_tokens()
     assert  Config.GOOGLE_APP_TOKENS == CONSTANT.GOOGLE_APP_TOKENS
 
+def test_retrieve_google_tokens_error():
+    Config._retrieve_google_tokens()
+    assert Config.GOOGLE_APP_TOKENS == (None, None)
+
 def test_retrieve_oauth_tokens(monkeypatch_setup):
     Config._retrieve_oauth_tokens()
     assert Config.FACEBOOK_APP_TOKENS == CONSTANT.FBOOK_APP_TOKENS
