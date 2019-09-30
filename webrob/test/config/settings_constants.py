a9163896c2d8c6343a62c2ce3d1c58c3f5c2f444
Rollson <rollsonipo@gmail.com>
2019-09-30 11:46:55 +0200

    refactor(settings_constants.py): use single quotes for string

--- webrob/test/config/settings_constants.py
+++ webrob/test/config/settings_constants.py
@@ -1,14 +1,16 @@
+# *********************Settings Test Constants**********************
+
 MAIL_PASSWORD = 'abc'
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = True
 MAIL_USE_SSL = True
 MAIL_USERNAME = 'Paul'
-FBOOK_APP_TOKENS = ("Paul01", "abCD123ef")
-TWIT_APP_TOKENS = ("Paul01", "abCDef123hi")
-GIT_APP_TOKENS = ("Paul01", "abCD-ef123hi")
-GOOGLE_APP_TOKENS = ("Paul01", "abCD-ef123hi/456")
-SERVICE_TOKENS = ("Paul01", "abCD-ef123hi/456")
+FBOOK_APP_TOKENS = ('Paul01', 'abCD123ef')
+TWIT_APP_TOKENS = ('Paul01', 'abCDef123hi')
+GIT_APP_TOKENS = ('Paul01', 'abCD-ef123hi')
+GOOGLE_APP_TOKENS = ('Paul01', 'abCD-ef123hi/456')
+SERVICE_TOKENS = ('Paul01', 'abCD-ef123hi/456')
 ROS_DISTRIBUTION = 'indigo'
 SQLALCHEMY_DATABASE_URI = 'postgresql://docker@127.17.0.82:5000/docker'
 POSTGRES_PORT_5432_TCP_ADDR = '127.17.0.82'
@@ -18,9 +20,9 @@ NULL_TOKENS = (None,None)
 DOCKBRIDGE_PORT_5001_TCP_ADDR = '170.17.0.60'
 DOCKBRIDGE_PORT_5001_TCP_PORT = '5001'
 
-URL = "http://170.17.0.60:5001"
+URL = 'http://170.17.0.60:5001'
 
-OAUTH_TOKENS = ("Paul01", "abCD-ef123hi/456")
+OAUTH_TOKENS = ('Paul01', 'abCD-ef123hi/456')
 
 OPENEASE_MESHES = 'git https://github.com/PR2/pr2_common,svn https://svn.com/PR2/pr2_common'
 MESH_REPOSITORIES = [('git', 'https://github.com/PR2/pr2_common'), ('svn', 'https://svn.com/PR2/pr2_common')]
