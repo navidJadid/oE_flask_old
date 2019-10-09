a07fd8d8ee7eaf34cff280699f23b45e5401aad4
Rollson <rollsonipo@gmail.com>
2019-10-09 13:37:58 +0200

    feat(tutorial_test_constant.py): add Tutorial test constants

---
+++ webrob/test/models/tutorial_test_constants.py
@@ -0,0 +1,12 @@
+# ********************TUTORIAL TEST CONSTANTS************************
+
+CAT_ID = 'robot programming'
+PAGE = 1
+CAT_TITLE = 'ros tutorial'
+TITLE_ONE = 'ros installation'
+TEXT_ONE = 'check ros documentation'
+
+TITLE_TWO = 'getting started'
+TEXT_TWO = 'installing and configuring ros workspace'
+
+
