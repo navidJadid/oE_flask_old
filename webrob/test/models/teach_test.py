f0430e091d9e44c1f9264ae6decfe058c2e1c4ca
Rollson <rollsonipo@gmail.com>
2019-10-09 21:27:56 +0200

    feat(teach_test.py): add function to create rows in database

---
+++ webrob/test/models/teach_test.py
@@ -0,0 +1,28 @@
+import pytest
+from webrob.app_and_db import app,db
+import teaching_test_constants as CONSTANTS
+from webrob.models.teaching import Course
+
+backup_config = app.config['SQLALCHEMY_DATABASE_URI']
+
+testDB = db
+
+def create_course_exercise_rows():
+    from webrob.models.teaching import CourseExercise
+    exercise1 = CourseExercise(course_id = CONSTANTS.COURSE_ID_ONE, number= CONSTANTS.COURSE_NUM_ONE,
+                               title= CONSTANTS.TITLE_ONE, archive= bin(20) )
+    exercise2 = CourseExercise(course_id = CONSTANTS.COURSE_ID_TWO, number = CONSTANTS.COURSE_NUM_TWO,
+                               title = CONSTANTS.TITLE_TWO, archive=bin(10))
+    testDB.session.add(exercise1)
+    testDB.session.add(exercise2)
+
+
+def create_course_task_rows():
+    from webrob.models.teaching import CourseTask
+    task1 = CourseTask(exercise_id = CONSTANTS.EXERCISE_ID_ONE, number = CONSTANTS.TASK_NUM_ONE,
+                       title = CONSTANTS.TASK_TITLE_ONE, text = CONSTANTS.TEXT_ONE)
+    task2 = CourseTask(exercise_id = CONSTANTS.EXERCISE_ID_ONE, number = CONSTANTS.TASK_NUM_ONE,
+                       title = CONSTANTS.TASK_TITLE_TWO, text = CONSTANTS.TEXT_TWO)
+    testDB.session.add(task1)
+    testDB.session.add(task2)
+
