fc5ab353f10fae07159c3ef603bddc79ddfc458d
Rollson <rollsonipo@gmail.com>
2019-10-09 21:56:37 +0200

    refactor(tutorial_test.py): move call to testDB.drop_all method to create_database function

--- webrob/test/models/tutorial_test.py
+++ webrob/test/models/tutorial_test.py
@@ -9,6 +9,7 @@ testDB = db
 def create_database():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test/models/tutotest.db'
 
+    testDB.drop_all()
     testDB.create_all()
     tut1 = Tutorial(cat_id = CONSTANTS.CAT_ID, cat_title = CONSTANTS.CAT_TITLE, title = CONSTANTS.TITLE_ONE,
                     text = CONSTANTS.TEXT_ONE, page = CONSTANTS.PAGE)
@@ -22,7 +23,6 @@ def test_read_tutorial_page():
     create_database()
     result = read_tutorial_page(CONSTANTS.CAT_ID, CONSTANTS.PAGE)
     assert result.title == CONSTANTS.TITLE_ONE
-    testDB.drop_all()
     app.config['SQLALCHEMY_DATABASE_URI'] = backup_config
 
 
