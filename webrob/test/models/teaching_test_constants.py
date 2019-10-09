8144ffec991527d3839042712f5650aaf8e29441
Rollson <rollsonipo@gmail.com>
2019-10-09 21:30:00 +0200

    feat(teaching_test_constant.py): add teaching test constants

---
+++ webrob/test/models/teaching_test_constants.py
@@ -0,0 +1,21 @@
+# ****************** TEACHING TEST CONSTANTS ***********************
+
+COURSE_ID_ONE = 100
+COURSE_NUM_ONE = 3
+TITLE_ONE = 'Computer Architecture'
+
+COURSE_ID_TWO = 101
+COURSE_NUM_TWO = 4
+TITLE_TWO = 'Object Oriented Programming with Python'
+
+EXERCISE_ID_ONE = 2
+TASK_NUM_ONE = 5
+TASK_TITLE_ONE = 'Hello world program in python'
+TEXT_ONE = 'write a python program that prints hello world to the console'
+
+TASK_TITLE_TWO = 'User Input program in python'
+TEXT_TWO = 'write a python program that accepts a user input data'
+
+
+
+
