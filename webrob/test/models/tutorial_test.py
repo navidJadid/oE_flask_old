3c5c6e52a474f2140d3bd617e21e7bd3f4e8cdf0
Rollson <rollsonipo@gmail.com>
2019-10-09 21:24:31 +0200

    refactor(tutorial_test.py): remove text fixture

--- webrob/test/models/tutorial_test.py
+++ webrob/test/models/tutorial_test.py
@@ -1,17 +1,14 @@
 import pytest
-from webrob.app_and_db import app
+from webrob.app_and_db import app, db
 import tutorial_test_constants as CONSTANTS
+from webrob.models.tutorials import Tutorial, read_tutorial_page
 
 backup_config = app.config['SQLALCHEMY_DATABASE_URI']
+testDB = db
 
-
-@pytest.fixture(scope="module")
 def create_database():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test/models/tutotest.db'
-    from webrob.app_and_db import db
-    from webrob.models.tutorials import Tutorial, read_tutorial_page
 
-    testDB = db
     testDB.create_all()
     tut1 = Tutorial(cat_id = CONSTANTS.CAT_ID, cat_title = CONSTANTS.CAT_TITLE, title = CONSTANTS.TITLE_ONE,
                     text = CONSTANTS.TEXT_ONE, page = CONSTANTS.PAGE)
@@ -20,16 +17,13 @@ def create_database():
     testDB.session.add(tut1)
     testDB.session.add(tut2)
     testDB.session.commit()
-    result = read_tutorial_page(CONSTANTS.CAT_ID, CONSTANTS.PAGE)
 
-    yield result
+def test_read_tutorial_page():
+    create_database()
+    result = read_tutorial_page(CONSTANTS.CAT_ID, CONSTANTS.PAGE)
+    assert result.title == CONSTANTS.TITLE_ONE
     testDB.drop_all()
     app.config['SQLALCHEMY_DATABASE_URI'] = backup_config
-
-
-def test_read_tutorial_page(create_database):
-    result = create_database
-    assert result.title == CONSTANTS.TITLE_ONE
 
 
 
