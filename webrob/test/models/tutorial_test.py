e20db68db8c552ea5c14a0472599c113b28925e0
Rollson <rollsonipo@gmail.com>
2019-10-14 11:22:19 +0200

    refactor(tutorial_test.py): add path to test database as a constant

--- webrob/test/models/tutorial_test.py
+++ webrob/test/models/tutorial_test.py
@@ -4,20 +4,19 @@ import tutorial_test_constants as CONSTANTS
 from webrob.models.tutorials import Tutorial, read_tutorial_page
 
 backup_config = app.config['SQLALCHEMY_DATABASE_URI']
-testDB = db
 
 def create_database():
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test/models/tutotest.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = CONSTANTS.TEST_DB_PATH
 
-    testDB.drop_all()
-    testDB.create_all()
+    db.drop_all()
+    db.create_all()
     tut1 = Tutorial(cat_id = CONSTANTS.CAT_ID, cat_title = CONSTANTS.CAT_TITLE, title = CONSTANTS.TITLE_ONE,
                     text = CONSTANTS.TEXT_ONE, page = CONSTANTS.PAGE)
     tut2 = Tutorial(cat_id = CONSTANTS.CAT_ID, cat_title = CONSTANTS.CAT_TITLE, title = CONSTANTS.TITLE_TWO,
                     text = CONSTANTS.TEXT_TWO, page = CONSTANTS.PAGE)
-    testDB.session.add(tut1)
-    testDB.session.add(tut2)
-    testDB.session.commit()
+    db.session.add(tut1)
+    db.session.add(tut2)
+    db.session.commit()
 
 def test_read_tutorial_page():
     create_database()
