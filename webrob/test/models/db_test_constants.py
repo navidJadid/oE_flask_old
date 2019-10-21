418c969cdf4de3fb79c0ce4c4b58abdb2cc79c5d
Rollson <rollsonipo@gmail.com>
2019-10-21 22:59:24 +0200

    refactor(db_test_constants.py): remove db import

--- webrob/test/models/db_test_constants.py
+++ webrob/test/models/db_test_constants.py
@@ -1,6 +1,5 @@
 # *******************DB Test Constants**********************
 
-from webrob.app_and_db import db
 from webrob.models.tutorials import Tutorial as Tutorial
 
 DB_COLUMNS = [{"name": "id", "type":Tutorial.id.type, "nullable":False}, {"name": "cat_id", "type":Tutorial.cat_id.type, "nullable":False}]
