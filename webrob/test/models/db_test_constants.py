3e92089c95e97f293a7341c2b815111a8bc298c7
Rollson <rollsonipo@gmail.com>
2019-10-21 23:09:27 +0200

    feat(db_test_constants.py): add column names

--- webrob/test/models/db_test_constants.py
+++ webrob/test/models/db_test_constants.py
@@ -3,4 +3,5 @@
 from webrob.models.tutorials import Tutorial as Tutorial
 
 DB_COLUMNS = [{"name": "id", "type":Tutorial.id.type, "nullable":False}, {"name": "cat_id", "type":Tutorial.cat_id.type, "nullable":False}]
+COLUMN_NAMES =['id', 'cat_id', 'cat_title', 'title', 'text', 'page']
 
