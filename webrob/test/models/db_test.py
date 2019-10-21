6c12eb9dbf1625a8e23abfc81e294435a577dfcb
Rollson <rollsonipo@gmail.com>
2019-10-21 23:08:19 +0200

    test(db_test.py): add test to retrieve db column names

--- webrob/test/models/db_test.py
+++ webrob/test/models/db_test.py
@@ -7,4 +7,8 @@ def test_db_columns():
     from webrob.models.db import db_columns
     columns = db_columns(Tutorial)
     assert columns[1] == CONSTANTS.DB_COLUMNS[1]
-    print 'priyanka'
+
+def test_db_column_names():
+    from webrob.models.db import db_column_names
+    column_names = db_column_names(Tutorial)
+    assert column_names == CONSTANTS.COLUMN_NAMES
