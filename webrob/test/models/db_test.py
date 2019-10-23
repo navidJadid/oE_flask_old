8f8f58b7081da05c9ca4c8e3691063ea8aa706c9
Rollson <rollsonipo@gmail.com>
2019-10-23 16:32:17 +0200

    test(db_test.py): add test for find_all method

--- webrob/test/models/db_test.py
+++ webrob/test/models/db_test.py
@@ -2,7 +2,6 @@ import  pytest
 from webrob.models.tutorials import Tutorial as Tutorial
 from webrob.test.models import db_test_constants as CONSTANTS
 
-
 def test_db_columns():
     from webrob.models.db import db_columns
     columns = db_columns(Tutorial)
@@ -12,3 +11,12 @@ def test_db_column_names():
     from webrob.models.db import db_column_names
     column_names = db_column_names(Tutorial)
     assert column_names == CONSTANTS.COLUMN_NAMES
+
+@pytest.mark.skip(reason="yet to resolve user_manager attribute problem")
+def test_db_find_all():
+    from webrob.app_and_db import app
+    with app.app_context():
+        from webrob.models.db import db_find_all
+        out = db_find_all(Tutorial)
+
+
