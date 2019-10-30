d9299adad1134251b14007de8e4f2bfe3a7f9266
Rollson <rollsonipo@gmail.com>
2019-10-30 16:12:07 +0100

    test(init_app_test.py): add test for valid password

--- webrob/test/startup/init_app_test.py
+++ webrob/test/startup/init_app_test.py
@@ -5,6 +5,13 @@ from webrob.app_and_db import app,db
 from webrob.test.startup import init_app_test_constants as CONSTANTS
 from flask_sqlalchemy import SQLAlchemy
 
+
+backup_config = app.config[CONSTANTS.DATABASE_URI]
+
 def test_for_null_password():
     msg = _check_password_and_display_message_on_error(app,CONSTANTS.NAME, None)
     assert msg == False
+
+def test_for_valid_password():
+    msg = _check_password_and_display_message_on_error(app, CONSTANTS.NAME, CONSTANTS.PASSWORD)
+    assert  msg == True
