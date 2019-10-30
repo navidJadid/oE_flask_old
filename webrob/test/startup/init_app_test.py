c121dae50a969d697308e3fad093a345eec0feba
Rollson <rollsonipo@gmail.com>
2019-10-30 16:03:08 +0100

    test(init_app_test.py): add test for null password

--- webrob/test/startup/init_app_test.py
+++ webrob/test/startup/init_app_test.py
@@ -1,42 +1,10 @@
-from webrob.startup.init_app import _check_password_and_display_message_on_error, _create_new_user_and_add_to_db, \
-    _append_roles_to_user_object
-
-from webrob.app_and_db import app, db
-from webrob.startup import init_app
-# from flask_user import UserManager
 import pytest
-import datetime
+from webrob.utility.db_connection_checker import got_db_connection
+from webrob.startup.init_app import _check_password_and_display_message_on_error
+from webrob.app_and_db import app,db
+from webrob.test.startup import init_app_test_constants as CONSTANTS
+from flask_sqlalchemy import SQLAlchemy
 
-
-class UserManagerMock:
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def hash_password(string):
-        return string
-
-@pytest.mark.skip(reason="will test later")
-def test_get_user():
-    from webrob.models.users import User
-    user_manager = UserManagerMock()
-    test_user = User(active=True,
-                     username='Paul',
-                     displayname='',
-                     remoteapp='',
-                     email='paul@example.com',
-                     confirmed_at=datetime.datetime.utcnow(),
-                     password=user_manager.hash_password('AAaa11'))
-    _append_roles_to_user_object(app, test_user, roles=[])
-
-    print 'test   = {}'.format(test_user)
-    return test_user
-
-@pytest.mark.skip(reason="will test later")
-def test_create_and_add_new_user():
-    # from webrob.startup.init_app import init_app
-    # init_app(app, db)
-    user_manager = UserManagerMock()
-    _create_new_user_and_add_to_db(app, db, user_manager, name='Paul', mail='paul@example.com', pw='AAaa11',
-                                   display_name='',
-                                   remote_app='', roles=[])
+def test_for_null_password():
+    msg = _check_password_and_display_message_on_error(app,CONSTANTS.NAME, None)
+    assert msg == False
