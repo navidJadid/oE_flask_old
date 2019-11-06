4cb7abc58eca949dc8eb555949a384874f68f910
Rollson <rollsonipo@gmail.com>
2019-11-06 16:22:49 +0100

    feat(init_db_test_constants.py): add path to test database

--- webrob/test/startup/init_db_test_constants.py
+++ webrob/test/startup/init_db_test_constants.py
@@ -1,4 +1,6 @@
 # **********************INIT_DB TEST CONSTANTS**********************
 
 DATABASE_URI = 'SQLALCHEMY_DATABASE_URI'
+TEST_DB_PATH = 'sqlite:///test/startup/dbTest.db'
+DB_FILE = 'dbTest.db'
 
