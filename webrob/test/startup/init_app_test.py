c22acede8a063d29fc7a7bdde8bacbbe91f326ba
Rollson <rollsonipo@gmail.com>
2019-11-17 21:32:24 +0100

    refactor(init_app_test.py): remove import for database connection method

--- webrob/test/startup/init_app_test.py
+++ webrob/test/startup/init_app_test.py
@@ -1,5 +1,5 @@
 import pytest
-from webrob.utility.db_connection_checker import got_db_connection
+
 from webrob.startup.init_app import _check_password_and_display_message_on_error
 from webrob.app_and_db import app,db
 from webrob.test.startup import init_app_test_constants as CONSTANTS
