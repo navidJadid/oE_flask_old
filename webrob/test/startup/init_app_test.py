ceb3a73bd5355dc6a0ffe78d88e02deed82cd14b
Rollson <rollsonipo@gmail.com>
2019-12-19 13:10:41 +0100

    chore(init_app_test.py): remove database engine mock Class

--- webrob/test/startup/init_app_test.py
+++ webrob/test/startup/init_app_test.py
@@ -1,29 +1,12 @@
 import pytest
 
+from webrob.app_and_db import app
 from webrob.startup.init_app import _check_password_and_display_message_on_error
-from webrob.app_and_db import app,db
+
 from webrob.test.startup import init_app_test_constants as CONSTANTS
-from flask_sqlalchemy import SQLAlchemy
+
 
 backup_config = app.config[CONSTANTS.DATABASE_URI]
-
-class MockDbEngineNormal:
-    def __init__(self):
-        self.cmd = "SELECT 1"
-
-    def execute(self, command):
-        # FIXME: Find way to remove hard-coding mock-method
-        if command == self.cmd:
-            return True
-        else:
-            raise Exception
-
-MOCK_DB_ENGINE = MockDbEngineNormal()
-
-@pytest.fixture
-def monkeypatch_setup(monkeypatch):
-    monkeypatch.setattr(SQLAlchemy, 'engine', MOCK_DB_ENGINE)
-    return monkeypatch
 
 def test_for_null_password():
     msg = _check_password_and_display_message_on_error(app,CONSTANTS.NAME, None)
@@ -32,3 +15,6 @@ def test_for_null_password():
 def test_for_valid_password():
     msg = _check_password_and_display_message_on_error(app, CONSTANTS.NAME, CONSTANTS.PASSWORD)
     assert  msg == True
+
+
+
